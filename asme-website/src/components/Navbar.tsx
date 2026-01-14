bfec37ab209a90534177f033254578a23c7e55bc
Tyler Tran <tylerttrann@gmail.com>
2026-01-14 12:48:31 -0800

    added asme png for icon

--- asme-website/src/components/Navbar.tsx
+++ asme-website/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 function Navbar() {
   return (
-    <nav className="font-helvetica bg-white/90 backdrop-blur-sm px-12 py-3 rounded-full shadow-lg border border-white/20">
-      <ul className="flex items-center gap-12 text-sm font-medium text-gray-800">
+    <nav className="font-helvetica font-medium bg-white/90 backdrop-blur-sm px-12 py-3 rounded-full shadow-lg border border-white/20">
+      <ul className="flex items-center gap-12 text-sm text-gray-800">
         <li className="cursor-pointer hover:text-blue-600 transition">About</li>
         <li className="cursor-pointer hover:text-blue-600 transition">Events</li>
         <li className="cursor-pointer hover:text-blue-600 transition">Yearbook</li>
