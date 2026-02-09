73f67de0024d377d40f9c4be46cd4a0c018e3177
Bronton Dieu <brontondieu1@gmail.com>
2026-02-08 22:01:56 -0800

    Added a basic grid placeholder

--- asme-website/src/pages/Events.tsx
+++ asme-website/src/pages/Events.tsx
@@ -5,11 +5,18 @@ function Events(){
     return(
         <Layout>
              <div className="p-32 bg-[#f1f0ea]">
-                <h1 className="text-[64px] font-bold text-blue-600 flex justify-center font-helvetica">Upcoming Events</h1>
-                <p className="mt-4 text-gray-700 flex justify-center">Check back here for ASME updates!</p>
+                <h1 className="text-[64px] font-bold text-blue-600 flex justify-center text-center font-helvetica">CHECK OUT OUR UPCOMING EVENTS!</h1>
     
                 {/* This is where you'll build your Instagram post section */}
+            </div>
+            <div className="container mx-auto p-15">
+                <h1 className="flex justify-center font-helvetica text-[40px] text-blue-400"> Posts </h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-32 bg-#ffffff">
+                    <div className="aspect-square bg-blue-100 p-15"> RNN JOCELYN SLAPS BRIAN </div>
+                    <div className="aspect-square bg-blue-100 p-15"> BRIAN TAKES REVENGE; PUSHES HER INTO POOL</div>
+                    <div className="aspect-square bg-blue-100 p-15"> Wow! RNN Recap </div>
                 </div>
+            </div>
         </Layout>
     )
 }
