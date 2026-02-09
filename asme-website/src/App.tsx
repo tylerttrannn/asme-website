a2f124f2e78b9dd759abd7acb1517877c05d21ec
Bronton Dieu <brontondieu1@gmail.com>
2026-02-08 21:01:16 -0800

    Added main header of events page

--- asme-website/src/App.tsx
+++ asme-website/src/App.tsx
@@ -25,6 +25,7 @@ export default function App() {
         <Route path="/" element={<FrontPage />} />
         <Route path="/links" element={<Links />} />
         <Route path="/board" element= {<Board/>} />
+        <Route path="/events" element = {<Events/>} />
         <Route path="*" element={<DefaultPage />} />
       </Routes>
     </BrowserRouter>
--- asme-website/src/pages/Events.tsx
+++ asme-website/src/pages/Events.tsx
@@ -1,18 +1,17 @@
-const Events = () => {
-  return (
-    <div className="bg-paper-texture min-h-screen p-8">
-      <h1 className="text-4xl font-bold text-blue-600">Upcoming Events</h1>
-      <p className="mt-4 text-gray-700">Check back here for ASME updates!</p>
-      
-      {/* This is where you'll build your Instagram post section */}
-      <div className="mt-10 grid gap-6">
-        <div className="bg-white p-6 rounded-lg shadow-md border-l-4 border-blue-500">
-          <h2 className="text-xl font-bold">Winter Quarter General Meetings </h2>
-          <p> Tuesdays @ 6:30 PM in MDEA </p>
-        </div>
-      </div>
-    </div>
-  );
-};
+import Layout from "../components/Layout"
+
+
+function Events(){
+    return(
+        <Layout>
+             <div className="p-32 bg-[#f1f0ea]">
+                <h1 className="text-[64px] font-bold text-blue-600 flex justify-center font-helvetica">Upcoming Events</h1>
+                <p className="mt-4 text-gray-700 flex justify-center">Check back here for ASME updates!</p>
+    
+                {/* This is where you'll build your Instagram post section */}
+                </div>
+        </Layout>
+    )
+}
 
 export default Events;
