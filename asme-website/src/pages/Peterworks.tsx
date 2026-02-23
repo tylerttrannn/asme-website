8424fc2bfde8ea19f539fc10ab31738d2dcd00d9
Bronton Dieu <brontondieu1@gmail.com>
2026-02-23 09:11:13 -0800

    created peterworks page

---
+++ asme-website/src/pages/Peterworks.tsx
@@ -0,0 +1,15 @@
+import Section from "../components/Section"
+import Layout from "../components/Layout"
+import { Reveal } from '../components/Reveal';
+
+function Peterworks(){
+    return(
+        <Layout>
+            <Section className="bg-[#f1f0ea]">
+                <div>
+                    
+                </div>
+            </Section>
+        </Layout>
+    )
+}
